612062ac3a93a971f049c9eea6824f05435c938a
nilcevc <nvilac@ucenfotec.ac.cr>
2023-05-30 22:58:48 -0600

    laboratorioParte01

---
+++ MyTestingApp/src/mytestingapp/MyTestingApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mytestingapp;
+ 
+
+/**
+ *
+ * @author gabyv
+ */
+public class MyTestingApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyForm myForm = new MyForm();
+        myForm.setVisible(true);
+    }
+    
+}
